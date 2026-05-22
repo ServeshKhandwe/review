9df7bffcd8d88ecc9d484d663ffcf0a6b32fa2e6
Servsh Khandwe <servesh1999@gmail.com>
2026-05-22 18:58:04 +0200

    CouponAppConfig: replace comment toggle with Spring profile annotations

--- src/main/java/it/schwarz/jobs/review/coupon/CouponAppConfig.java
+++ src/main/java/it/schwarz/jobs/review/coupon/CouponAppConfig.java
@@ -8,19 +8,21 @@ import it.schwarz.jobs.review.coupon.provider.jpa.CouponJpaRepository;
 import it.schwarz.jobs.review.coupon.provider.jpa.JpaCouponProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class CouponAppConfig {
 
     @Bean
+    @Profile("!inmem")
     public CouponProvider getJpaCouponProvider(
             CouponJpaRepository couponJpaRepository,
             ApplicationJpaRepository applicationRepository) {
         return new JpaCouponProvider(couponJpaRepository, applicationRepository);
     }
 
-    // Comment in/out one of the CouponProvider Beans to select one for runtime
-    // @Bean
+    @Bean
+    @Profile("inmem")
     public CouponProvider getInMemCouponProvider() {
         return new InMemoryCouponProvider();
     }
