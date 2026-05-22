e695b4d7a832bac985a6b69ad626d990ecac0c93
Servsh Khandwe <servesh1999@gmail.com>
2026-05-22 19:24:44 +0200

    CouponExceptionHandler: return generic 500, log exception internally

--- src/main/java/it/schwarz/jobs/review/coupon/api/CouponExceptionHandler.java
+++ src/main/java/it/schwarz/jobs/review/coupon/api/CouponExceptionHandler.java
@@ -1,6 +1,8 @@
 package it.schwarz.jobs.review.coupon.api;
 
 import it.schwarz.jobs.review.coupon.domain.usecase.BusinessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
@@ -14,6 +16,8 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class CouponExceptionHandler extends ResponseEntityExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(CouponExceptionHandler.class);
+
     @ExceptionHandler(value = {BusinessException.class})
     protected ResponseEntity<Object> handleBusinessExceptions(BusinessException ex, WebRequest request) {
         final var problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
@@ -25,8 +29,9 @@ public class CouponExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {Exception.class})
     protected ResponseEntity<Object> handleAllOtherExceptions(Exception ex, WebRequest request) {
+        log.error("Unhandled exception", ex);
         final var problemDetail = ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR);
-        problemDetail.setDetail(ex.getMessage());
+        problemDetail.setDetail("An unexpected error occurred. Please try again later.");
 
         return handleExceptionInternal(ex, problemDetail,
                 new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
